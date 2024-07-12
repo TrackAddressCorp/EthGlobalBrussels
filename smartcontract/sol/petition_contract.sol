83bb5649710e80976e5ff574832d5ebb76a7817f
Leon David Zipp <leondavidzipp@gmx.de>
2024-07-12 22:58:01 +0200

    fixed syntax and type errors

--- smartcontract/sol/petition_contract.sol
+++ smartcontract/sol/petition_contract.sol
@@ -10,25 +10,30 @@ contract Petition {
         string  gender;
     }
 
-    string private petition_name;
     string private petition_title;
+    string private petition_text;
     Signer[] private signers;
     uint256 private signers_count;
 
-    constructor(string memory _petition_name, string memory _petition_title) {
-        petition_name = _petition_name;
+    constructor(string memory _petition_title, string memory _petition_text) {
         petition_title = _petition_title;
+        petition_text = _petition_text;
         signers_count = 0;
     }
 
-    function sign(string _world_id) public {
+    function sign(string memory _world_id) public {
         for (uint i = 0; i < signers.length; i++) {
-            string id = signers[i].world_id;
-            if (_world_id == msg.sender) {
+            if (keccak256(abi.encodePacked(signers[i].world_id)) == keccak256(abi.encodePacked(_world_id))) {
                 revert("You have already signed this petition.");
             }
         }
-        signers.push(msg.sender);
+        signers.push(
+            Signer(
+                _world_id,
+                block.timestamp,
+                "N/A",
+                0,
+                "N/A"));
         signers_count++;
     }
 
@@ -45,6 +50,6 @@ contract Petition {
     }
 
     function get_petition_text() public view returns (string memory) {
-        return petition_name;
+        return petition_text;
     }
 }
