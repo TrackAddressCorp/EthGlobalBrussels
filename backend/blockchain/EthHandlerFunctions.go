d77c9f07dbbd5c380d5c076216fc7fabe9069959
Leon David Zipp <leondavidzipp@gmx.de>
2024-07-13 10:57:00 +0000

    fixed import error, renamed GetNextTransaction -> GetAuth

--- backend/blockchain/EthHandlerFunctions.go
+++ backend/blockchain/EthHandlerFunctions.go
@@ -3,25 +3,23 @@ package blockchain
 import (
 	"context"
 	"crypto/ecdsa"
-	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 )
 
-type GetNextTransactionParams struct {
+type GetAuthParams struct {
     PrivateKey    *ecdsa.PrivateKey `json:"private_key"`
     PublicAddress common.Address    `json:"public_address"`
     ChainID       *big.Int          `json:"chain_id"`
 }
 
-func (h *Handler) GetNextTransaction(ctx context.Context, params *GetNextTransactionParams) (*bind.TransactOpts, error) {
+func (h *Handler) GetAuth(ctx context.Context, params *GetAuthParams) (*bind.TransactOpts, error) {
     nonce, err := h.Client.PendingNonceAt(ctx, params.PublicAddress)
     if err != nil {
         return &bind.TransactOpts{}, err
     }
-
 
     // auth, err := bind.NewKeyedTransactorWithChainID(params.PrivateKey, params.ChainID)
     // if err != nil {
@@ -58,39 +56,46 @@ func (h *Handler) GetNextTransaction(ctx context.Context, params *GetNextTransac
 
 type DeployParams struct {
     PrivateKey      *ecdsa.PrivateKey   `json:"private_key"`
+    PublicAddress   common.Address       `json:"public_address"`
     PetitionTitle   string              `json:"petition_title"`
     PetitionText    string              `json:"petition_text"`
 }
 
-func (h *Handler) DeployPetition(ctx context.Context, params DeployParams) (string, error) {
+type DeployResult struct {
+    Address     common.Address      `json:"address"`
+    Transaction string              `json:"transaction"`
+    Contract    *PetitionContract   `json:"contract"`
+}
+
+func (h *Handler) DeployPetition(ctx context.Context, params DeployParams) (*DeployResult, error) {
     chainID, err := h.Client.ChainID(ctx)
     if err != nil {
-        return "", err
+        return &DeployResult{}, err
     }
-    auth, err := h.GetNextTransaction(
-		ctx, &GetNextTransactionParams{
-			Hash: params.Hash,
-			PrivateKey: params.PrivateKey,
-			PublicAddress: params.PublicAddress,
-			ChainID: chainID,
-		},
-	)
-	if err != nil {
-		fmt.Println("error 2:", err)
-		return &UploadEthCertificateResult{}, err
-	}
+    auth, err := h.GetAuth(
+        ctx, &GetAuthParams{
+            PrivateKey: params.PrivateKey,
+            PublicAddress: params.PublicAddress,
+            ChainID: chainID,
+        },
+    )
+    if err != nil {
+        return &DeployResult{}, err
+    }
 
-	address, transaction, contract, err := DeployCertificateContract(
-		auth,
-		h.client,
-		params.Hash,
-		params.IssuerName,
-		params.RecipientName,
-	)
-    auth.Nonce = big.NewInt(int64(nonce))
-    auth.Value = big.NewInt(0)     // in wei
-    auth.GasLimit = uint64(300000) // in units
-    auth.GasPrice = gasPrice
-    DeployPetitionContract(eth, params.PetitionTitle, params.PetitionText)
-    return "", nil
+    address, transaction, contract, err := DeployPetitionContract(
+        auth,
+        h.Client,
+        params.PetitionTitle,
+        params.PetitionText,
+    )
+    if err != nil {
+        return &DeployResult{}, nil
+    }
+
+    return &DeployResult{
+        Address: address,
+        Transaction: transaction.Hash().Hex(),
+        Contract: contract,
+    }, nil
 }
