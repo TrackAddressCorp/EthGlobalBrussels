bc0739fd8c0059e79194bea1c91903ccfc1168e1
jojomo96 <48690914+jojomo96@users.noreply.github.com>
2024-07-13 23:25:39 +0200

    make petitions links

--- frontendv3/src/components/petitionCard.tsx
+++ frontendv3/src/components/petitionCard.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, Flex, Spacer, LinkBox, LinkOverlay } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
 
-const PetitionCard = ({ key, title, description }: {key :number,  title: string, description: string }) => {
+const PetitionCard = ({ id, title, description, signs }: { id: number, title: string, description: string, signs: number }) => {
+  const router = useRouter();
+  // Function to handle navigation
+  const navigateToPetition = () => {
+    router.push(`/petition/${id}`);
+  };
+
   return (
-    <Box
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-      p={4}
-      boxShadow="md"
-      bg="white"
-      key={key}
-      width={{ base: '100%', md: 'auto' }}
-    >
-      <Heading as="h3" size="lg" mb={4}>
-        {title}
-      </Heading>
-      <Text fontSize="md">
-        {description}
-      </Text>
-    </Box>
+    <LinkBox as="article" onClick={navigateToPetition} style={{ cursor: 'pointer' }}> {/* Make the whole card clickable */}
+      <Box
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        p={4}
+        boxShadow="md"
+        bg="white"
+        height="400px" // Fixed height
+        overflowY="auto" // Allows scrolling within the card for overflow content
+      >
+        <Flex justifyContent="space-between" alignItems="center" mb={4}>
+          <Heading as="h3" size="lg">
+            <LinkOverlay href={`/petition/${id}`}>{title}</LinkOverlay> {/* Use LinkOverlay for better accessibility */}
+          </Heading>
+          <Text fontSize="sm" color="gray.500">
+            {signs} Signs
+          </Text>
+        </Flex>
+        <Text fontSize="md" mb={4}>
+          {description}
+        </Text>
+      </Box>
+    </LinkBox>
   );
 };
 
