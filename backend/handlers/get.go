230137015f12f5c22a6c9a996a2fa28a468f7d1f
Paulic <61714149+PaulicStudios@users.noreply.github.com>
2024-07-13 11:03:32 +0000

    changed all responses to json

--- backend/handlers/get.go
+++ backend/handlers/get.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 
 	"github.com/TrackAddressCorp/EthGlobalBrussels/db"
+	"github.com/TrackAddressCorp/EthGlobalBrussels/models"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,12 +13,12 @@ func GetPetition(c *fiber.Ctx) error {
 
 	intID, err := strconv.Atoi(id)
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid ID")
+		return c.Status(fiber.StatusBadRequest).JSON(models.Response{StatusMsg: "Invalid ID", StatusCode: fiber.StatusBadRequest})
 	}
 
 	partition, err := db.GetPetition(uint(intID))
 	if err != nil {
-		return c.Status(fiber.StatusNotFound).SendString("Partition not found")
+		return c.Status(fiber.StatusNotFound).JSON(models.Response{StatusMsg: "Petition not found", StatusCode: fiber.StatusNotFound})
 	}
 
 	return c.JSON(partition)
--- backend/handlers/list.go
+++ backend/handlers/list.go
@@ -2,13 +2,14 @@ package handlers
 
 import (
 	"github.com/TrackAddressCorp/EthGlobalBrussels/db"
+	"github.com/TrackAddressCorp/EthGlobalBrussels/models"
 	"github.com/gofiber/fiber/v2"
 )
 
 func ListPetitions(c *fiber.Ctx) error {
 	petitions, err := db.ListPetitions()
 	if err != nil {
-		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+		return c.Status(fiber.StatusForbidden).JSON(models.Response{StatusMsg: err.Error(), StatusCode: fiber.StatusForbidden})
 	}
 
 	return c.JSON(petitions)
