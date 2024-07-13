6ae9a449f136cc616f81000d32948247d1540d10
Leon David Zipp <leondavidzipp@gmx.de>
2024-07-13 12:48:08 +0000

    added sign test

--- backend/blockchain/EthHandlerFunctions_test.go
+++ backend/blockchain/EthHandlerFunctions_test.go
@@ -8,21 +8,41 @@ import (
 )
 
 var params DeployParams
+var result *DeployResult
 
 func TestDeployContract(t *testing.T) {
-	ctx := context.Background()
+    ctx := context.Background()
 
-	params = DeployParams{
-		PetitionTitle: "Test Petition",
-		PetitionText: "This is a test petition",
-	}
-	result, err := testHandler.DeployPetition(
-		ctx,
-		params,
-	)
-	require.NoError(t, err)
-	require.NotEmpty(t, result)
-	require.NotZero(t, result.Address)
-	require.NotZero(t, result.Contract)
-	require.NotEmpty(t, result.Transaction)
+    params = DeployParams{
+        PetitionTitle: "Test Petition",
+        PetitionText: "This is a test petition",
+    }
+
+    var err error
+    result, err = testHandler.DeployPetition(
+        ctx,
+        params,
+    )
+
+    require.NoError(t, err)
+    require.NotEmpty(t, result)
+    require.NotZero(t, result.Address)
+    require.NotZero(t, result.Contract)
+    require.NotEmpty(t, result.Transaction)
+}
+
+func TestSignPetition(t *testing.T) {
+    ctx := context.Background()
+
+    params := SignParams{
+        PetitionAddress: result.Address,
+        WorldID: "TestWorldID",
+    }
+
+    err := testHandler.SignPetition(
+        ctx,
+        params,
+    )
+
+    require.NoError(t, err)
 }
