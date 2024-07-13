5b2c0469e9f25904373a6eb4e7a9b960654244db
jojomo96 <48690914+jojomo96@users.noreply.github.com>
2024-07-13 20:14:40 +0200

    v3

---
+++ frontendv3/src/pages/about.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is an example of an About page in a Next.js application.</p>
+    </div>
+  );
+}
